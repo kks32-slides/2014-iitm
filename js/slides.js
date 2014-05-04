46b2802262eaf2f4743430af4eb5dc8d17dbe3fc
Krishna Kumar <kks32@gmx.com>
2014-05-04 23:00:09 +0530

    Mathjax local

--- js/slides.js
+++ js/slides.js
@@ -3,3 +3,18 @@ require(['order!../slide_config', 'order!modernizr.custom.45394',
          'order!slide-deck'], function(someModule) {
 
 });
+
+/* Load scripts depending on mode of access (local vs remote). */
+var lscripts = null;
+if (localmode) {
+  logmsg("Starting in LOCAL ACCESS mode.");
+  loadscripts("localscript");
+  var forceload_cmd = "loadscripts('remotescript');";
+  logmsg('To forceload remote scripts, <span onclick="'
+         +forceload_cmd+'" style="text-decoration:underline;'
+         +'cursor:pointer;">click here</span>.');
+} else {
+  logmsg("Starting in REMOTE ACCESS mode.");
+  loadscripts("remotescript");
+}
+
--- slide_config.js
+++ slide_config.js
@@ -2,35 +2,35 @@ var SLIDE_CONFIG = {
   // Slide settings
   settings: {
     title: 'Multi-Scale Multiphase Modelling of Granular Flows',
-	//	subtitle: 'Material Point Method and Discrete Element - Lattice Boltzmann Coupling',
-    //eventInfo: {
-    // title: 'Google I/O',
-    // date: '6/x/2013'
-    //},
-    useBuilds: true, // Default: true. False will turn off slide animation builds.
-    usePrettify: true, // Default: true
-    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
-    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/theme/kings_icon.png',
-    fonts: [
-      'Open Sans:regular,semibold,italic,italicsemibold',
-      'Source Code Pro'
-    ],
-    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
-    
-    //Workshop 
-    workshop: '21 November 2013, Cambridge',
+      //	subtitle: 'Material Point Method and Discrete Element - Lattice Boltzmann Coupling',
+      //eventInfo: {
+      // title: 'Google I/O',
+      // date: '6/x/2013'
+      //},
+      useBuilds: true, // Default: true. False will turn off slide animation builds.
+      usePrettify: true, // Default: true
+      enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+      enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+      //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+      favIcon: 'images/theme/kings_icon.png',
+      fonts: [
+          'Open Sans:regular,semibold,italic,italicsemibold',
+          'Source Code Pro'
+      ],
+      //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+      
+      //Workshop 
+      workshop: '8th May 2014, IIT Madras',
   },
-
-  // Author information
-  presenters: [{
-    name: 'Krishna Kumar',
-    company: '',
-    email: 'kks32@cam.ac.uk'
-  }, {
-    name: 'Prof. Kenichi Soga',
-    company: 'University of Cambridge, UK',
-  }]
+    
+    // Author information
+    presenters: [{
+        name: 'Krishna Kumar',
+        company: '',
+        email: 'kks32@cam.ac.uk'
+    }, {
+        name: 'Prof. Kenichi Soga',
+        company: 'University of Cambridge, UK',
+    }]
 };
 
